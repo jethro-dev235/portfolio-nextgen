6f660c43a3303a0fad22b7511b6aca49a596a159
Baoledji Dingam Jethro <baoledjijethro@gmail.com>
2026-05-13 23:27:04 +0100

    Create Home component for portfolio page

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Mon Portfolio Next.js 🚀</h1>
+      <p>Site déployé sur GitHub Pages</p>
+    </main>
+  );
+}
