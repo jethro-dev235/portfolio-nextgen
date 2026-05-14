d57dfb690905125f22223dda7b40a9404a4a8f34
Baoledji Dingam Jethro <baoledjijethro@gmail.com>
2026-05-14 09:10:06 +0100

    Configure base path and asset prefix for Next.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/portfolio-nextgen',
+  assetPrefix: '/portfolio-nextgen/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
